59d15b6da7a5b033d17c2ff43e8e3db3cdcca09d
Noah Vargas <noah.vargas@daviestech.org>
2020-10-05 12:50:37 +0000

    Finished Assignment

---
+++ silly_sentences.py
@@ -0,0 +1,18 @@
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+adjective1 = input("Enter an adjective: ")
+food1 = input("Enter a food: ")
+food2 = input("Enter a food: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter an adverb: ")
+noun = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+
+print ("Lets play Silly Sentences!")
+
+print(name + " was planning a dream vacation to " + place + ".")
+print(name + " was especially looking forward to trying the local cuisine, including " + adjective1 + " " + food1 + " and " + food2 + ".")
+
+print(name + " will have to practice the language " + adverb + " to make it easier to " + verb + " with people.")
+
+print(name + " has a long list of sights to see, including the " + noun + " museum and the " + adjective2 + " park.")
